772049a1a2b378dc1f17b3ebae438ce9e075fb97
Zakharov Vladymyr <twins88odessa@gmail.com>
2018-01-27 22:06:04 +0200

    add BottomNavigationView

--- app/src/main/java/com/example/twins/meeter/ui/MainActivity.java
+++ app/src/main/java/com/example/twins/meeter/ui/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.twins.meeter.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.twins.meeter.R;
-import com.example.twins.meeter.ui.activities.LoginActivity;
 import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,12 +18,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FacebookSdk.sdkInitialize(getApplicationContext());
 
-        findViewById(R.id.login_view).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//                startActivity(intent);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_favorites:
+                                Toast.makeText(MainActivity.this, "click action_favorites", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.action_likes:
+                                Toast.makeText(MainActivity.this, "click action_favorites", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.action_setting:
+                                Toast.makeText(MainActivity.this, "click action_favorites", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                        return true;
+                    }
+                });
     }
 }
