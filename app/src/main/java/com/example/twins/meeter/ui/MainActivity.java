092a74c078a320f9cf428d6483c316007292bf2d
Zakharov Vladymyr <twins88odessa@gmail.com>
2018-01-27 22:26:22 +0200

    add ToolBar

--- app/src/main/java/com/example/twins/meeter/ui/MainActivity.java
+++ app/src/main/java/com/example/twins/meeter/ui/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.example.twins.meeter.R;
 import com.facebook.FacebookSdk;
@@ -18,10 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FacebookSdk.sdkInitialize(getApplicationContext());
 
-//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//                startActivity(intent);
-
-
+//      Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//      startActivity(intent);
+        final TextView titleView = findViewById(R.id.title_tool_bar);
+        titleView.setText(R.string.favorite);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -29,13 +29,13 @@ public class MainActivity extends AppCompatActivity {
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
                             case R.id.action_favorites:
-                                Toast.makeText(MainActivity.this, "click action_favorites", Toast.LENGTH_SHORT).show();
+                                titleView.setText(R.string.favorite);
                                 break;
                             case R.id.action_likes:
-                                Toast.makeText(MainActivity.this, "click action_favorites", Toast.LENGTH_SHORT).show();
+                                titleView.setText(R.string.feed);
                                 break;
                             case R.id.action_setting:
-                                Toast.makeText(MainActivity.this, "click action_favorites", Toast.LENGTH_SHORT).show();
+                                titleView.setText(R.string.settings);
                                 break;
                         }
                         return true;
