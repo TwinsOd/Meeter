5f535ed21a161c768a0ae072bebf3065e720c11b
Zakharov Vladymyr <twins88odessa@gmail.com>
2018-01-27 23:37:03 +0200

    make basic logic

--- app/src/main/java/com/example/twins/meeter/ui/activityMain/MainActivity.java
+++ app/src/main/java/com/example/twins/meeter/ui/activityMain/MainActivity.java
@@ -8,9 +8,14 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.twins.meeter.R;
+import com.example.twins.meeter.ui.account.AccountFragment;
+import com.example.twins.meeter.ui.favorites.FavoritesFragment;
+import com.example.twins.meeter.ui.feed.FeedFragment;
+import com.example.twins.meeter.ui.settings.SettingsFragment;
 import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,8 +28,17 @@ public class MainActivity extends AppCompatActivity {
 
 //      Intent intent = new Intent(MainActivity.this, LoginActivity.class);
 //      startActivity(intent);
+
         final TextView titleView = findViewById(R.id.title_tool_bar);
         titleView.setText(R.string.favorite);
+        onShowFragment(FavoritesFragment.newInstance(), false);
+        (findViewById(R.id.avatar_view)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                titleView.setText(R.string.account);
+                onShowFragment(AccountFragment.newInstance(), true);
+            }
+        });
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -33,12 +47,15 @@ public class MainActivity extends AppCompatActivity {
                         switch (item.getItemId()) {
                             case R.id.action_favorites:
                                 titleView.setText(R.string.favorite);
+                                onShowFragment(FavoritesFragment.newInstance(), false);
                                 break;
-                            case R.id.action_likes:
+                            case R.id.action_feed:
                                 titleView.setText(R.string.feed);
+                                onShowFragment(FeedFragment.newInstance(), false);
                                 break;
                             case R.id.action_setting:
                                 titleView.setText(R.string.settings);
+                                onShowFragment(SettingsFragment.newInstance(), false);
                                 break;
                         }
                         return true;
